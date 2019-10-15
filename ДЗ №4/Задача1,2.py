4d1b7fd004781be1881a454a11b640491dd4cd53
1 <alenakrutikova2@gmail.com>
2019-10-15 15:27:23 +0500

    Доделано второе задание

--- ДЗ №4/Задача1,2.py
+++ ДЗ №4/Задача1,2.py
@@ -9,10 +9,10 @@ ls = []
 def CreateBinaryFile(filename: str, ls, a, b, size_ls):
     file = open(filename, 'wb')
     for i in range(size_ls):
-        ls.append(random.randint(a, b))
-    print(ls)    
+        ls.append(random.randint(a, b))   
     file.write(struct.pack('>' + 'l' * size_ls, *ls))
     file.close()
+    return ls
     
 def ReadBinaryFile(filename: str, ls, a, b, size_ls) -> list:
     file = open(filename, 'rb')
@@ -21,6 +21,15 @@ def ReadBinaryFile(filename: str, ls, a, b, size_ls) -> list:
     file.close()
     return list(txt)
 
-CreateBinaryFile('Бинарный файл', ls, a, b, size_ls)
+ls = CreateBinaryFile('Бинарный файл', ls, a, b, size_ls)
 print(ReadBinaryFile('Бинарный файл', ls, a, b, size_ls))
+
+max_value = max(ls)
+min_value = min(ls)
+average_value = sum(ls) / len(ls)
+ls.sort()
+mediana = ls[len(ls) // 2]
+
+print(max_value, min_value, average_value, mediana)
+
 
